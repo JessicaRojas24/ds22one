a48d1c485068b6462043492004a57b659568a2c2
Jessica Rojas <101826380+JessicaRojas24@users.noreply.github.com>
2022-06-01 13:18:36 -0500

    Update 2.2tree-problems.js

--- ds-xy-js-master/3p/2trees/2linked-/2do/2.2tree-problems.js
+++ ds-xy-js-master/3p/2trees/2linked-/2do/2.2tree-problems.js
@@ -25,3 +25,74 @@
  * posorder(root)
  * 
  */
+class Node {
+  constructor(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+
+  show() {
+    return this.data;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  insert(data) {
+    var n = new Node(data, null, null);
+    if (this.root == null) {
+      this.root = n;
+    } else {
+      var current = this.root;
+      var parent;
+      while (true) {
+        parent = current;
+        if (data < current.data) {
+          current = current.left;
+          if (current == null) {
+            parent.left = n;
+            break;
+          }
+        } else {
+          current = current.right;
+          if (current == null) {
+            parent.right = n;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  inOrder(node = this.root) {
+    if (!(node == null)) {
+      this.inOrder(node.left);
+      console.log(node.show() + " ");
+      this.inOrder(node.right);
+    }
+  }
+
+  postOrder(node = this.root) {
+    if (!(node == null)) {
+      this.postOrder(node.left);
+      this.postOrder(node.right);
+      console.log(node.show() + " ");
+    }
+  }
+}
+
+var nums = new BST();
+nums.insert(23);
+nums.insert(45);
+nums.insert(16);
+nums.insert(37);
+nums.insert(3);
+nums.insert(99);
+nums.insert(22);
+console.log("Inorder traversal: ");
+nums.inOrder(nums.root);
+console.log("Postorder traversal: ");
+nums.postOrder(nums.root);
