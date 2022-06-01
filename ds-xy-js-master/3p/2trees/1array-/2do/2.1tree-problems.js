d72bdc482d01814c0497b91e7419b29c0dfdb1e8
Jessica Rojas <101826380+JessicaRojas24@users.noreply.github.com>
2022-06-01 13:17:38 -0500

    Update 2.1tree-problems.js

--- ds-xy-js-master/3p/2trees/1array-/2do/2.1tree-problems.js
+++ ds-xy-js-master/3p/2trees/1array-/2do/2.1tree-problems.js
@@ -10,3 +10,73 @@
  * 
  * write it and make sure it runs
  */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  toString() {
+    let str = `${this.data} => `;
+    this.children.forEach((child) => {
+      str += child.data + " ";
+    });
+    return str;
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  add(data, toNodeData) {
+    const node = new Node(data);
+    const parent = toNodeData ? this.findBFS(toNodeData) : null;
+
+    if (parent) {
+      parent.children.push(node);
+    } else {
+      if (!this.root) {
+        this.root = node;
+      } else {
+        return "error";
+      }
+    }
+  }
+
+  findBFS(data) {
+    let _node = null;
+
+    this.traverseBFS((node) => {
+      if (node.data == data) {
+        _node = node;
+      }
+    });
+    return _node;
+  }
+
+  traverseBFS(cb) {
+    const queue = [this.root];
+
+    if (cb) {
+      while (queue.length) {
+        const node = queue.shift();
+
+        cb(node);
+
+        for (const child of node.children) {
+          queue.push(child);
+        }
+      }
+    }
+  }
+}
+
+let tree = new Tree();
+tree.add("A"); 
+tree.add("B", "A"); 
+tree.add("C", "A");
+tree.add("D", "B");
+tree.add("E", "B");
+tree.traverseBFS((node) => console.log(node.toString())); 
