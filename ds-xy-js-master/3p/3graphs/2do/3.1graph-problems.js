eb5924c6c961ba20990585a7c6d41582d69168b8
Jessica Rojas <101826380+JessicaRojas24@users.noreply.github.com>
2022-06-01 13:20:19 -0500

    Update 3.1graph-problems.js

--- ds-xy-js-master/3p/3graphs/2do/3.1graph-problems.js
+++ ds-xy-js-master/3p/3graphs/2do/3.1graph-problems.js
@@ -10,24 +10,79 @@
  *
  * write it and make sure it runs
  */
-let directed =  [
-    [0, 1, 0, 1, 0, 0], 
-    [0, 0, 0, 0, 1, 0], 
-    [0, 0, 0, 0, 1, 1], 
-    [0, 1, 0, 0, 0, 0], 
-    [0, 0, 0, 1, 0, 0], 
-    [0, 0, 0, 0, 0, 1], 
-]
-let undirected =  [
-    [0, 1, 0, 0, 1], 
-    [1, 0, 1, 1, 1], 
-    [0, 1, 0, 1, 0], 
-    [0, 1, 1, 0, 1], 
-    [1, 1, 0, 1, 0], 
-]
-let tree =  [
-    [0, 1, 0, 0],
-    [1, 0, 1, 1],
-    [0, 1, 0, 1],
-    [0, 1, 1, 0],
-]
+let tree = [
+  [0, 1, 0, 0], //1
+  [1, 0, 1, 1], //2
+  [0, 1, 0, 0], //3
+  [0, 1, 0, 0], //4
+];
+
+class Matriz {
+  constructor(matriz) {
+    this.matriz = matriz;
+  }
+
+  isSquare(matriz) {
+    for (let i = 0; i < matriz.length; i++) {
+      let evaluar;
+      evaluar = matriz[i].length;
+
+      if (evaluar != matriz.length) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  isSymmetrical(matriz) {
+    for (let i = 0; i < matriz.length; i++) {
+      for (let j = 0; j < matriz[i].length; j++) {
+        if (matriz[i][j] != matriz[j][i]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  isTree(matriz) {
+    let sum = 0;
+    let contador = 0;
+    let columnas = 0;
+
+    for (let i = 0; i < matriz.length; i++) {
+      for (let j = 0; j < matriz[i].length; j++) {
+        contador++;
+        columnas = contador / matriz.length;
+      }
+    }
+
+    for (let i = 1; i < matriz.length; i++) {
+      sum = 0;
+      for (let j = 0; j < columnas; j++) {
+        if (j < i) {
+          sum += matriz[i][j];
+          if (sum > 1) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+
+  isGraph(matriz) {
+    if (matriz.isTree(matriz) == false) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+let matriz = new Matriz(tree);
+
+console.log(matriz.isSquare(matriz));
+console.log(matriz.isSymmetrical(matriz));
+console.log(matriz.isTree(matriz));
+console.log(matriz.isGraph(matriz));
